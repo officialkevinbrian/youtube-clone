af7e3fea541006a98ef35218acc0354af6960406
officialkevinbrian <kevinbrian@cortex-moz.com>
2024-07-10 12:45:22 +0200

    Update app/(app)/user.tsx

--- app/(app)/user.tsx
+++ app/(app)/user.tsx
@@ -1,5 +1,6 @@
 import UserContentSection from "@/components/user/userContentSection";
 import UserTopSection from "@/components/user/userTopSection";
+import { supabase } from "@/config/supabase.config";
 import { AuthContext } from "@/providers/AuthProvider";
 import { Stack } from "expo-router";
 import React, { useContext } from "react";
@@ -17,6 +18,18 @@ const UserProfileScreen = () => {
 
       <View flex={1}>
         <UserTopSection />
+        <XStack p={"$2"}>
+          <Button
+            onPress={async () => {
+              // await supabase.auth.signOut();
+              alert("Under Development");
+            }}
+            bg={"black"}
+            color={"white"}
+          >
+            SignOut
+          </Button>
+        </XStack>
         <UserContentSection />
       </View>
     </>
